af84c5ceba6fa235a9c8ea3f13fd0aad5396f660
valhache <valhache@93cdc80c-8133-432f-9df4-c4b43f13cebb>
2015-12-08 17:29:03 +0000

    Creation of contactList-merge branch

---
+++ branches/contactList-merge/Contact.js
@@ -0,0 +1,157 @@
+/**
+ * Contact class
+ * Définit un objet contact en renseignant son prénom, nom, entreprise,
+ * fonction, numéros de téléphone(fixe et portable) et son email.
+ *
+ * @class Contact
+ * @property {String} firstName Prénom du contact
+ * @property {String} lastName Nom du contact
+ * @property {Array} organisation Entreprise(s) dans laquelle travaille le contact
+ * @property {Array} title Fonction(s) du contact
+ * @property {Object} phone Numéro de téléphone fixe (travail ou domicile) du contact
+ * @property {String} cellPhone Numéro de téléphone portable du contact
+ * @property {String} email Adresse mail du contact
+ */
+function Contact() {
+    var firstName, lastName, organisation,
+            title, phone, cellPhone, email;
+    Object.defineProperties(this, {
+        "firstName": {
+            get: function () {
+                return firstName;
+            },
+            set: function (value) {
+                var matched = value.match(/[\wàéèêîù'\s]+/i);
+                if (matched) {
+                    firstName = matched[0];
+                } else {
+                    throw {name: "firstNameValue", type: "error", message: "Le format du Prénom est incorrect"};
+                }
+            }
+        },
+        "lastName": {
+            get: function () {
+                return lastName;
+            },
+            set: function (value) {
+                var matched = value.match(/[\wàéèêîù'\s]+/i);
+                if (matched) {
+                    lastName = matched[0];
+                } else {
+                    throw {name: "lastNameValue", type: "error", message: "Le format du Nom est incorrect"};
+                }
+            }
+        },
+        "organisation": {
+            get: function () {
+                return organisation;
+            },
+            set: function (value) {
+                organisation = [];
+                for (var i = 0; i < value.length; i++) {
+                    var matched = value[i].match(/[\wàéèêîù'\s]+/i);
+                    if (matched) {
+                        organisation.push(matched[0]);
+                    } else {
+                        throw {name: "orgValue", type: "error", message: "Le format de/des Compagnie(s) est incorrect"};
+                    }
+                }
+            }
+        },
+        "title": {
+            get: function () {
+                return title;
+            },
+            set: function (value) {
+                title = [];
+                for (var i = 0; i < value.length; i++) {
+                    var matched = value[i].match(/[\wàéèêîù'\s]+/i);
+                    if (matched) {
+                        title.push(matched[0]);
+                    } else {
+                        throw {name: "titleValue", type: "error", message: "Le format de la/des fonction(s) est incorrect"};
+                    }
+                }
+            }
+        },
+        "phone": {
+            get: function () {
+                return phone;
+            },
+            set: function (value) {
+                phone = {};
+                var matched = value['number'].match(/^(\+\d{11})$/);
+                if (matched) {
+                    phone = value;
+                } else {
+                    throw {name: "phoneValue", type: "error", message: "Le format du numéro de Téléphone fixe est incorrect"};
+                }
+            }
+        },
+        "cellPhone": {
+            get: function () {
+                return cellPhone;
+            },
+            set: function (value) {
+                var matched = value.match(/^(\+\d{11})$/);
+                if (matched) {
+                    cellPhone = matched[0];
+                } else {
+                    throw {name: "cellPhoneValue", type: "error", message: "Le format du numéro de Téléphone Portable est incorrect"};
+                }
+            }
+        },
+        "email": {
+            get: function () {
+                return email;
+            },
+            set: function (value) {
+                var matched = value.match(/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i);
+                if (matched) {
+                    email = matched[0];
+                } else {
+                    throw {name: "emailValue", type: "error", message: "Le format de l'Adresse Mail est incorrect"};
+                }
+            }
+        }
+    });
+}
+
+Contact.prototype.toJSON = function () {
+    var obj = {
+        NOM: this.lastName,
+        PRENOM: this.firstName,
+        ORGANISATION: this.organisation,
+        FONCTION: this.title,
+        TELEPHONE: this.phone,
+        MOBILE: this.cellPhone,
+        EMAIL: this.email
+    };
+    return JSON.stringify(obj);
+};
+
+Contact.prototype.toVCardString = function () {
+    var vCardArray = [
+        "BEGIN:VCARD",
+        "VERSION:4.0",
+        "N:" + this.lastName + ";" + this.firstName,
+        "ORG:" + this.organisation.join(";"),
+        "TITLE:" + this.title.join(";"),
+        "TEL;TYPE=" + this.phone["type"] + ",voice;VALUE=uri:tel:" + this.phone["number"],
+        "TEL;TYPE=cell,voice;VALUE=uri:tel:" + this.cellPhone,
+        "EMAIL:" + this.email,
+        "END:VCARD"
+    ];
+    return vCardArray.join("\r\n");
+};
+
+Contact.prototype.toString = function () {
+    return "Nom Contact : " + this.firstName.verbose + " " + this.lastName.verbose +
+            "\nCompagnie : " + this.organisation.join(', ').verbose +
+            "\nFonction : " + this.title.join(', ').verbose +
+            "\nNuméro Téléphone (" + this.phone.type + ") : " + this.phone.number.verbose +
+            "\nNuméro Portable : " + this.cellPhone.verbose +
+            "\nEmail : " + this.email.verbose;
+};
+
+module.exports = Contact;
---
+++ branches/contactList-merge/main.js
@@ -0,0 +1,78 @@
+/**
+ * @file Fichier principal main.js
+ * @source: https://sourceforge.net/projects/dionysos-gl02/
+ *
+ * @author Yassine DOGHRI <yassine.doghri@utt.fr>
+ * @author Valentin HACHET <valentin.hachet@utt.fr>
+ * @author Youssef Nassim AZIZ <youssef_nassim.aziz@utt.fr>
+ * @author Ayoub BAKKALI EL KASMI <ayoub.bakkali_el_kasmi@utt.fr>
+ * 
+ * Copyright (C) 2015 Atlantis
+ */
+
+var prettyjson = require('prettyjson');
+var chalk = require('chalk');
+var prettyJsonOptions = {
+    noColor: false
+};
+var fs = require('fs');
+var vCardParser = require('./vCardParser.js');
+var Contact = require('./Contact.js');
+var ContactList = require('./ContactList.js');
+var file = "test/sample_c.vcf";
+fs.readFile(file, 'utf8', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
+
+    analyzer = new vCardParser(data);
+    analyzer.parseToJSON();
+    var jsonData = analyzer.jsonData;
+    var contactList = new ContactList();
+    for (var i = 0; i < jsonData.length; i++) {
+        try {
+            var contact = new Contact();
+            contact.firstName = jsonData[i]['firstName'];
+            contact.lastName = jsonData[i]['lastName'];
+            contact.organisation = jsonData[i]['organisation'];
+            contact.title = jsonData[i]['title'];
+            contact.phone = jsonData[i]['phone'];
+            contact.cellPhone = jsonData[i]['cellPhone'];
+            contact.email = jsonData[i]['email'];
+            contactList.addContact(contact);
+        } catch (e) {
+            handleException(e);
+        }
+    }
+
+//    console.log(prettyjson.render(contactList, prettyJsonOptions));
+//    try {
+//        var duplicateContacts = contactList.duplicates();
+//        console.log(duplicateContacts);
+//        for (var j in duplicateContacts) {
+//            console.log(j + "\r\n" + duplicateContacts[j]);
+//        }
+//    } catch (e) {
+//        handleException(e);
+//    }
+
+    // Si on a des contacts similaires (à tester avec sample_c.vcf)
+    try {
+        contactList.displayConflicts();
+    } catch (e) {
+        handleException(e);
+    }
+});
+function handleException(e) {
+    var error = chalk.white.bgRed.bold;
+    var errorMsg = chalk.bold.red;
+    var info = chalk.white.bgBlue.bold;
+    var infoMsg = chalk.bold.blue;
+    if (e.type === "info") {
+        console.log(info(" " + e.type + " ") + " " + infoMsg(e.message));
+    } else {
+        console.log(error(" " + e.type + " ") + " " + errorMsg(e.message));
+        console.log(chalk.gray("Stoping process..."));
+        process.exit();
+    }
+}
